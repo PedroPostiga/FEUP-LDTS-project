75a3a3e5f3ab53185812c8f163a2b5a84a407b9a
Filipe Carvalho Cruz <up202404158@up.pt>
2025-12-09 18:02:56 +0000

    menu and game controller

--- src/main/java/com/gladiator/controller/Controller.java
+++ src/main/java/com/gladiator/controller/Controller.java
@@ -1,18 +1,52 @@
 package com.gladiator.controller;
 
-import com.gladiator.Application;
 import com.gladiator.gui.GUI;
-import com.gladiator.view.game.ArenaViewer;
-
 import java.io.IOException;
 
-public class Controller {
-    private ArenaViewer arenaViewer;
+public abstract class Controller {
 
-    public Controller(ArenaViewer arenaViewer){
-        this.arenaViewer = arenaViewer;
+    protected boolean running = true;
+    protected final GameTimer timer;
+
+    protected Controller(int ticksPerSecond) {
+        this.timer = new GameTimer(ticksPerSecond);
     }
-    public void step(GUI gui) throws IOException {
-        arenaViewer.draw(gui);
+
+    public final void run(GUI gui) throws IOException {
+        while (running) {
+            long startTime = System.currentTimeMillis();
+
+            processInput(gui);
+
+            if (timer.shouldTick()) {
+                update();
+            }
+
+            draw(gui);
+            limitFPS(startTime, 60);
+        }
+
+        gui.close();
+    }
+
+    protected abstract void processInput(GUI gui) throws IOException;
+    protected abstract void update();
+    protected abstract void draw(GUI gui) throws IOException;
+
+    protected void stop() {
+        running = false;
+    }
+
+    private void limitFPS(long startTime, int targetFPS) {
+        long frameTime = System.currentTimeMillis() - startTime;
+        long targetFrameTime = 1000 / targetFPS;
+
+        if (frameTime < targetFrameTime) {
+            try {
+                Thread.sleep(targetFrameTime - frameTime);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
